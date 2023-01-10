5ff74333b7ffc1ab1d3f115b6dbea9a3d024f9ca
Christian-Encalada <cris_7715@hotmail.com>
2023-01-10 01:55:30 -0500

    crear correo

--- app/src/main/java/com/chris/proyecto_prilad/Pantalla_2.kt
+++ app/src/main/java/com/chris/proyecto_prilad/Pantalla_2.kt
@@ -4,11 +4,26 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Pantalla_2 : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla2)
+
+        val btnsesion : Button = findViewById(R.id.iniciarSesionBtn)
+        val txtemail : TextView = findViewById(R.id.edtCorreo)
+        val txtpass : TextView = findViewById(R.id.edtContrasena)
+        //botones de pantallas
 
         val btn: Button = findViewById(R.id.iniciarSesionBtn)
         btn.setOnClickListener {
@@ -16,11 +31,38 @@ class Pantalla_2 : AppCompatActivity() {
             startActivity(intent)
 
 
-            }
+        }
         val btn2: Button = findViewById(R.id.registroBtn)
         btn2.setOnClickListener {
             val intent: Intent = Intent(this, RegisterAcivity::class.java)
             startActivity(intent)
         }
+
+
+        //iniciar sesion
+        firebaseAuth= Firebase.auth
+        btnsesion.setOnClickListener()
+        {
+            singIn(txtemail.text.toString(), txtpass.text.toString())
+        }
+
+
+
     }
+    private fun singIn(email: String, password: String)
+    {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val user = firebaseAuth.currentUser
+                    Toast.makeText(baseContext, user?.uid.toString(), Toast.LENGTH_SHORT).show()
+                    // aqui vamos a ir a la segunda acivity
+                }
+                else
+                {
+                    Toast.makeText(baseContext,"Error de Email y/o Contraseña" , Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+
 }
--- app/src/main/java/com/chris/proyecto_prilad/RegisterAcivity.kt
+++ app/src/main/java/com/chris/proyecto_prilad/RegisterAcivity.kt
@@ -1,20 +1,71 @@
 package com.chris.proyecto_prilad
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterAcivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_acivity)
-
-        val btn2: Button = findViewById(R.id.registroTerminadoBtn)
-        btn2.setOnClickListener {
-            val intent: Intent = Intent(this, Pantalla_2::class.java)
-            startActivity(intent)
-
+        val txtemail_nuevo : TextView = findViewById(R.id.edtCorreo2)
+        val txtpass_nuevo : TextView = findViewById(R.id.edtContrasena)
+        val txtpass_nuevo_conf : TextView = findViewById(R.id.edtContrasenaConfirmacion)
+        val btnCrear : Button = findViewById(R.id.registroTerminadoBtn)
+        btnCrear.setOnClickListener(){
+            var pass1 = txtpass_nuevo.text.toString()
+            var pass2 = txtpass_nuevo_conf.text.toString()
+            if (pass1.equals(pass2))
+            {
+                crearCuenta(txtemail_nuevo.text.toString(), txtpass_nuevo.text.toString())
+            }
+            else
+            {
+                Toast.makeText(baseContext, "Las contraseñas no son iguales",Toast.LENGTH_SHORT).show()
+                txtpass_nuevo.requestFocus()
+            }
         }
+
+        //registro
+        firebaseAuth= Firebase.auth
+
+
+
+
+        //Botones
+        //val btn2: Button = findViewById(R.id.registroTerminadoBtn)
+       // btn2.setOnClickListener {
+          //  val intent: Intent = Intent(this, Pantalla_2::class.java)
+          //  startActivity(intent)
+
+        //}
     }
+
+    private fun crearCuenta(email: String, password: String){
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this)  {task ->
+                if (task.isSuccessful)
+                {
+                    Toast.makeText(baseContext, "Cuenta creada", Toast.LENGTH_SHORT).show()
+                }
+                else
+                {
+                    Toast.makeText(baseContext, "Error al crear la cuenta: " + task.exception, Toast.LENGTH_SHORT).show()
+
+                }
+            }
+
+    }
+
 }
+
